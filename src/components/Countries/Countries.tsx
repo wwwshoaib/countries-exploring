1b4eca8275b3cb97dd9df16b72e66d068776a96b
Shoaib <shoaibchemistrysust@gmail.com>
2026-09-11 09:40:55 +0600

    using 'state' visited countries were counted, event handler was used to lift props from parent to child

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,4 +1,4 @@
-import { use } from "react"
+import { use, useState } from "react"
 import type { CountryType } from "../../files/CountryType"
 import '../Countries/Countris.css'
 import Country from "../Country/Country";
@@ -8,16 +8,38 @@ export interface CountriesProps {
 }
 
 export default function Countries({ countriesPromise }: CountriesProps) {
-
+    //load the countries data from the promise using the use hook
     const countries = use(countriesPromise);
-    
+    //create a state to store the visited countries
+    const [visitedCountries, setVisitedCountries] = useState<CountryType[]>([]);
+
+    const handleVisitedCountries = (country: CountryType): void => {
+        if (visitedCountries.includes(country)) {
+            const remainingCountries = visitedCountries.filter(c => c !== country);
+            setVisitedCountries(remainingCountries);
+
+        }
+        else {
+            const newVisitedCountries = [...visitedCountries, country];
+            setVisitedCountries(newVisitedCountries);
+        }
+    }
+
+
 
     return (
         <>
             <h1>Countries in world</h1>
+            <h2>Visited Countries: {visitedCountries.length}</h2>
             <div className="countries">
                 {
-                    countries.map(country => <Country key = {country.ccn3.ccn3} country = {country} ></Country>)
+                    countries.map(country =>
+                        <Country
+                            key={country.ccn3.ccn3}
+                            country={country}
+                            handleVisitedCountries={handleVisitedCountries} >
+
+                        </Country>)
                 }
 
             </div>
--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,18 +1,30 @@
+
+import { useState } from "react"
 import type { CountryType } from "../../files/CountryType"
 import '../Country/Country.css'
 
 export interface CountryProps {
     country: CountryType,
+    handleVisitedCountries: (country: CountryType) => void
 }
 
-export default function Country({ country }: CountryProps) {
-    
+export default function Country({ country, handleVisitedCountries }: CountryProps) {
+
+    const [visited, setVisited] = useState<boolean>(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+        handleVisitedCountries(country);
+    }
+
+
     return (
-        <div className="country">
+        <div className={`country ${visited ? 'country-visited' : ''}`}>
             <h4>{country.name.common}</h4>
             <img src={country.flags.flags.png} alt="" />
             <p>Capital: {country.capital.capital}</p>
             <p>Capital: {country.population.population}</p>
+            <button onClick={handleVisited} >{(visited) ? 'Visited' : 'Mark as visited'}</button>
         </div>
     )
-}
+}  
